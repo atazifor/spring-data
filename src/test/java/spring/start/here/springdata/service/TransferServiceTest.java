3079feae722872aebedd3222609bdbe3b8a4de2a
Amin Tazifor <amin.tazifor@gmail.com>
2024-09-15 08:43:11 +0100

    mockito test exceptions thrown

--- src/test/java/spring/start/here/springdata/service/TransferServiceTest.java
+++ src/test/java/spring/start/here/springdata/service/TransferServiceTest.java
@@ -5,15 +5,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import spring.start.here.springdata.exceptions.AccountNotFoundException;
 import spring.start.here.springdata.model.Account;
 import spring.start.here.springdata.repository.AccountRepository;
 
 import java.math.BigDecimal;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class) //allow use of @Mock and @InjectMocks
 class TransferServiceTest {
@@ -45,5 +46,21 @@ class TransferServiceTest {
         //validations
         verify(accountRepository).changeAmount(1l, new BigDecimal(50));
         verify(accountRepository).changeAmount(2l, new BigDecimal(250));
+    }
+
+    @Test
+    void moneyTransferDestinationAccountNotFoundFlow() {
+        Account sender = new Account();
+        sender.setId(1);
+        sender.setAmount(new BigDecimal(1000));
+        sender.setName("Sender");
+
+        given(accountRepository.findById(1l)).willReturn(Optional.of(sender));
+        given(accountRepository.findById(2L)).willReturn(Optional.empty());
+
+        assertThrows(AccountNotFoundException.class,
+                () -> transferService.transferMoney(1, 2, new BigDecimal(50)));
+
+        verify(accountRepository, never()).changeAmount(anyLong(), any());
     }
 }
